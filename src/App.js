09245435a9429aa606f5792bda576dc9186d6e68
john-nash-rs <rohitsingh20121992@gmail.com>
2023-08-18 18:43:45 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+// src/App.js
+
+import React from 'react';
+import './App.css';
+import LoginForm from './components/LoginForm';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Welcome to Our Interactive Login Page</h1>
+      <LoginForm />
+    </div>
+  );
+}
+
+export default App;
